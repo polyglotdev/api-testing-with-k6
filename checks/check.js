e3e1e7f60ed5e85689ac871e45f4cc2727631987
Dom Hallan <dom@domhallan.com>
2024-05-09 21:58:09 -0400

    Added basic API checks
    Implemented a new check.js file to perform basic health checks on the API. The checks include verifying that the response status is 2xx, ensuring the response body is not empty, and confirming that the response duration is less than 500ms.

---
+++ checks/check.js
@@ -0,0 +1,16 @@
+import http from 'k6/http'
+import { check } from 'k6'
+
+const url = `http://localhost:8888/alphamart/api/basic/fast`
+
+export default function () {
+  let response = http.get(url)
+
+  console.log(`Response ${response.body}`)
+
+  check(response, {
+    'response status is 2xx': (r) => r.status === 200 && r.status < 300,
+    'response body is not empty': (r) => r.body.length > 0,
+    'response duration < 500ms': (r) => r.timings.duration < 500
+  })
+}
