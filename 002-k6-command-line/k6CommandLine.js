9cb4be709f0c0a23a843d7598049710fa6d64115
Dom Hallan <dom@domhallan.com>
2024-05-09 20:43:00 -0400

    Enhanced logging for k6 command line tests
    Added detailed logging to the k6 command line test script. Now, each iteration logs its duration in seconds, the virtual user ID, and the total time that the script has been running. This will provide more insight into how long each test is taking and which virtual user is executing it.

--- 002-k6-command-line/k6CommandLine.js
+++ 002-k6-command-line/k6CommandLine.js
@@ -3,5 +3,13 @@ import { sleep } from 'k6'
 
 export default function () {
   http.get('https://test.k6.io')
-  sleep(1)
+
+  const durationInSeconds = (exec.instance.currentTest.duration / 1000).toFixed(
+    2
+  )
+  console.log(
+    `Iteration ${exec.scenario.iterationInTest} Duration: ${durationInSeconds} seconds` +
+      ` VU: ${exec.vu.idInTest}` +
+      `Script has been running for: ${durationInSeconds} seconds`
+  )
 }
