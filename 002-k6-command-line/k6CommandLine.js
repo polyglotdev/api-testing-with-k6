9da4c29126754fc534acea05ec3b1d0a24e725f7
Dom Hallan <dom@domhallan.com>
2024-05-09 21:06:46 -0400

    Refactored k6 command line script
    The k6 command line script has been significantly refactored. The http.get method now targets a different URL and the response is checked to ensure it returns a 200 status code. Removed unnecessary logging of test run duration and other details.

--- 002-k6-command-line/k6CommandLine.js
+++ 002-k6-command-line/k6CommandLine.js
@@ -1,14 +1,9 @@
 import http from 'k6/http'
-import { exec } from 'k6'
+import { check } from 'k6'
 
 export default function () {
-  http.get('https://www.google.com')
-
-  const durationInSeconds = (exec.instance.currentTestRunDuration / 1000).toFixed(2)
-
-  console.log(
-    `Iteration ${exec.scenario.iterationInTest},` +
-      `VU: ${exec.vu.idInTest},` +
-      `Script has been running for: ${durationInSeconds} seconds`
-  )
+  const res = http.get('https://httpbin.test.k6.io')
+  check(res, {
+    'response code was 200': (res) => res.status == 200
+  })
 }
