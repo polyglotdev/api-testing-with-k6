761e8d037a9322a9579f876166d8cfdd65273059
Dom Hallan <dom@domhallan.com>
2024-05-09 21:14:17 -0400

    Updated sleep function with randomIntBetween
    The sleep function in the main script has been updated to use the 'randomIntBetween' method from k6-utils. This change replaces the previous Math.random() method, providing a more controlled range of delay between 20 and 30 seconds.

--- 003-sleep/sleep.js
+++ 003-sleep/sleep.js
@@ -1,8 +1,9 @@
 import { sleep } from 'k6'
 import http from 'k6/http'
+import { randomIntBetween } from 'https://jslib.k6.io/k6-utils/1.2.0/index.js'
 
 export default function () {
   http.get('https://k6.io')
-  sleep(Math.random() * 2)
+  sleep(randomIntBetween(20, 30))
   http.get('https://k6.io/features')
 }
